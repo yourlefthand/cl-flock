3484c44d756373017fb428f8731bd87f992d2273
kal <yourlefthand@gmail.com>
2015-06-12 20:49:58 -0600

    got local search & using a flattened image of the world down should be able to create higher resolution images

--- kernal.cl
+++ kernal.cl
@@ -1,49 +1,47 @@
-
-
-__kernel void flock(
-	__global int4* pos,
-	__global int* mass,
-	__global int* power,
-	__global int* lifetime,
+__kernel void knn(
+	__global int8* starling,
 	__global int* world,
-	__global int4* out,
-	int inner_rad, int outer_rad,
+	__global int8* out,
 	int world_size_x, int world_size_y
 	)
 {
 	unsigned int gid = get_global_id(0);
 
-	// int4 collector;
+	private int3 separation;
+	private int3 cohesion;
+	private int3 alignment;
 
-	// int2 goal;
+	private int2 p = starling[gid].s01;
 
-	// float d;
+	private int inner_rad = starling[gid].s2;
+	private int outer_rad = starling[gid].s3;
 
-	// int2 p = pos[gid].lo;
-	// int2 v = pos[gid].hi;
+	for(int i = (-1 * outer_rad);i <= outer_rad; i++){
+		int y = p.y + i;
+		int row = y * world_size_y;
+		for(int k = (-1 * outer_rad); k <= outer_rad; k++){
+			int x = p.x + k;
+			int column = row + x;
+			if (row >= 0 && row < world_size_y && column >= row && column < row + world_size_y){
+				if (world[column] > 0){
+					private int2 neighbor;
+					neighbor.x = x + p.x;
+					neighbor.y = y + p.y;
 
-	// int j = power[gid];
-	// int l = lifetime[gid];
-	// int m = mass[gid];
+					float d = distance(convert_float2(p),convert_float2(neighbor));
+					
+					if(d <= outer_rad && d > inner_rad){
 
-	// uint n_cohere;
-	// uint2 p_cohere;
+					}
+					if(d <= inner_rad && d > 0){
 
-	// uint n_separate;
-	// uint2 p_separate;
-
-	// for(int i = (-1 * outer_rad);i <= outer_rad; i++){
-	// 	int y = p.y + i;
-	// 	int row = y * world_size_y;
-	// 	for(int k = (-1 * outer_rad); k <= outer_rad; k++){
-	// 		int x = p.x + k;
-	// 		int column = row + x;
-	// 		if (world[column] > 0){
-	// 		}
-	// 	}
-	// }
-
-//	collector.z = gid;
-	out[gid].x = 0;
-	out[gid].y = 0;
+					}
+				}
+				else {
+					out[gid].s0 = 0;
+					out[gid].s1 = 0;
+				}
+			}
+		}	
+	}
 }
--- main.py
+++ main.py
@@ -1,77 +1,83 @@
-import sys, cProfile
+import sys
 
 import pyopencl as cl
 
 import numpy as np
 import scipy.misc as scp
 
-def do_cprofile(func):
-    def profiled_func(*args, **kwargs):
-        profile = cProfile.Profile()
-        try:
-            profile.enable()
-            result = func(*args, **kwargs)
-            profile.disable()
-            return result
-        finally:
-            profile.print_stats()
-    return profiled_func
-
 def init_data(num, res_x, res_y):
-    pos = np.zeros((num, 4), dtype=np.int32)
+    pos = np.zeros((num, 8), dtype=np.int32)
 
-    #pos[:,0] = np.arange(num) % np.sqrt(num)
     pos[:,0] = np.arange(num) % res_x
     pos[:,0] *= np.random.random_sample((num,))
 
     pos[:,1] = np.arange(num) % res_y
     pos[:,1] *= np.random.random_sample((num,))
 
-    pos[:,2] = 0
-    pos[:,3] = 0
+    pos[:,2] = 3
+    pos[:,3] = 9
 
-    mass = np.zeros(num, dtype=np.int32)
-    mass[:] = 12
+    return pos
 
-    power = np.zeros(num, dtype=np.int32)
-    power[:] = 36
+def draw(world, count):
+    scp.imsave("./out/image/frame_{0:05d}.png".format(count),world.astype(bool))
 
-    lifetime = np.zeros(num, dtype=np.int32)
-    lifetime[:] = 10
-
-    return pos, mass, power, lifetime
+def form_world(accel_vector, x_size, y_size):
+    world = np.zeros((x_size, y_size), dtype=np.int32)
+    world[accel_vector[:,0],accel_vector[:,1]] = 1
+    return world
 
 class KDTree(object):
     """requires dataset with length of rational sqrt for now"""
-    def __init__(self, min_leaf_size=2):
-        self.min_leaf_size = min_leaf_size
-    def recurse(self, data, shape, dimension):
-        if shape[0] % self.min_leaf_size == 0 and shape[1] % self.min_leaf_size == 0:
-            med = np.divide(shape[dimension],2)
-            left = np.where((data[:,dimension] <= med))
-            self.tree.append(left[:])
-            right = np.where((data[:,dimension] >  med))
-            self.tree.append(right[:])
-            left_data  = data[left]
-            self.literal.append(left_data[:])
-            right_data = data[right]
-            self.literal.append(right_data[:])
-            shape = shape[:]
-            shape[dimension] = med
-            dimension += 1
-            if dimension > 1:
-                dimension = 0
-            self.recurse(left_data, shape, dimension)
-            self.recurse(right_data, shape, dimension)
+    def __init__(self):
+        self.null = None
 
-    def execute(self, data, shape):
-        self.tree = []
-        self.literal = []
-        dimension = 0
+    def form_tree(self, data, mins=None, maxs=None):
+        self.data = np.asarray(data)
 
-        self.recurse(data, shape, dimension)
+        assert self.data.shape[1] == 2
 
-        return self.tree, self.literal
+        if mins is None:
+            mins = data.min(0)
+        if maxs is None:
+            maxs = data.max(0)
+
+        self.mins = np.asarray(mins)
+        self.maxs = np.asarray(maxs)
+        self.size_s = self.maxs - self.mins
+
+        self.child1 = None
+        self.child2 = None
+
+        if len(data) > 1:
+            # sort on the dimension with the largest spread
+            largest_dim = np.argmax(self.size_s)
+            i_sort = np.argsort(self.data[:, largest_dim])
+            new_data = self.data[i_sort, :]
+
+            # find split point
+            N = new_data.shape[0]
+            split_point = 0.5 * (new_data[N / 2, largest_dim]
+                                 + new_data[N / 2 - 1, largest_dim])
+
+            # create subnodes
+            mins1 = self.mins.copy()
+            mins1[largest_dim] = split_point
+            maxs2 = self.maxs.copy()
+            maxs2[largest_dim] = split_point
+
+            # Recursively build a KD-tree on each sub-node
+            self.child1 = self.form_tree(new_data[N / 2:], mins=mins1, maxs=self.maxs)      
+            self.child2 = self.form_tree(new_data[:N / 2], mins=self.mins, maxs=maxs2)
+
+        return np.asarray([data, self.child1, self.child2])
+
+    def tree(self, data):
+        tree = []
+
+        np.append(tree, self.form_tree(data))
+
+        return tree
 
 
 class OpenCl(object):
@@ -97,25 +103,19 @@ class OpenCl(object):
         program = cl.Program(self.ctx, fstr).build()
         return program
 
-    def cl_load_data(self, pos, mass, power, lifetime, world):
+    def cl_load_data(self, pos, world):
         mf = cl.mem_flags
 
-        world_cl = cl.Buffer(self.ctx, mf.READ_ONLY | mf.COPY_HOST_PTR, hostbuf=world.flatten())
+        world_cl = cl.Buffer(self.ctx, mf.READ_ONLY | mf.COPY_HOST_PTR, hostbuf=world.flatten());
 
         pos_cl = cl.Buffer(self.ctx, mf.READ_ONLY | mf.COPY_HOST_PTR, hostbuf=pos)
-        mass_cl = cl.Buffer(self.ctx, mf.READ_ONLY | mf.COPY_HOST_PTR, hostbuf=mass)
-        power_cl = cl.Buffer(self.ctx, mf.READ_ONLY | mf.COPY_HOST_PTR, hostbuf=power)
-        lifetime_cl = cl.Buffer(self.ctx, mf.READ_ONLY | mf.COPY_HOST_PTR, hostbuf=lifetime)
 
         out = cl.Buffer(self.ctx, mf.WRITE_ONLY, pos.nbytes)
 
-        return pos_cl, mass_cl, power_cl, lifetime_cl, world_cl, out
+        return pos_cl, world_cl, out
 
-    def execute(self, pos, mass, power, lifetime, world):
-        pos_cl, mass_cl, power_cl, lifetime_cl, world_cl, out = self.cl_load_data(pos, mass, power, lifetime, world)
-
-        inner_rad = np.int32(1)
-        outer_rad = np.int32(3)
+    def execute(self, pos, world):
+        pos_cl, world_cl, out = self.cl_load_data(pos, world)
 
         world_x = np.int32(world.shape[0])
         world_y = np.int32(world.shape[1])
@@ -125,83 +125,45 @@ class OpenCl(object):
         global_size = (self.num,)
         local_size = None
 
-        kernalargs = (pos_cl,
-                      mass_cl,
-                      power_cl,
-                      lifetime_cl,
+        kernalargs = (
+                      pos_cl,
                       world_cl,
                       out,
-                      inner_rad, outer_rad,
                       world_x, world_y
-                      )
+                     )
 
-        self.program.flock(self.queue, global_size, local_size, *(kernalargs)).wait()
+        self.program.knn(self.queue, global_size, local_size, *(kernalargs)).wait()
         cl.enqueue_read_buffer(self.queue, out, ret)
-        #finish not needed with wait
         #self.queue.finish()
         return ret
 
-def form_world(accel_vector, x_size, y_size):
-    world = np.zeros((x_size, y_size), dtype=np.int32)
-    world[accel_vector[:,0],accel_vector[:,1]] = 1
-    return world
-
-def draw(image,count):
-    scp.imsave("./out/image/frame_{0:05d}.png".format(count),image.astype(bool))
-    #dimen = np.sqrt(num)
-    #world = np.zeros((dimen,dimen))
-    #for starling in pos:
-    #    if starling[0] < dimen and starling[0] > 0 and starling[1] < dimen and starling[1] > 0:
-    #        world[starling[0],starling[1]] = True
-    #scp.imsave("./out/image/frame_{0:05d}.png".format(count),world.astype(bool))
-
-
 if __name__ == "__main__":
-    np.set_printoptions(threshold=np.nan,linewidth=1024)
-
     sys.settrace
     sys.setrecursionlimit(1024**2)
-    
-    num = 1024**2
-    resolution = [1024,1024]
 
-    #num = 1280 * 720
-    #resolution = [1280,720]
-    
-    #num = 64 * 64
-    #resolution = [64,64]
+    #num = 1024**2
+    #resolution = [1024,1024]
 
-    #num = 640 * 480
-    #resolution = [640, 480]
-
-    #num = 200*200
-    #resolution = [200,200]
-
-    print num
-    print resolution
+    num = 1280 * 720
+    resolution = [720, 1280]
 
     dt = .001
 
+    starlings = init_data(num, resolution[0], resolution[1])
+    world = form_world(starlings, resolution[0],resolution[1])
+
     opcl = OpenCl(num, dt)
-
-    pos, mass, power, lifetime = init_data(num,resolution[0],resolution[1])
-
-    world = form_world(pos,resolution[0],resolution[1])
-
-    #kd = KDTree(min_leaf_size=4)
-    #kd.execute(pos, resolution)   
 
     count = 0
     
     while True:
-        #draw(world, count)
-
-        result = opcl.execute(pos, mass, power, lifetime, world)
-        print result
-        #world = form_world(result,resolution[0],resolution[1])
-
-        #print world
-        print count
-        #kd.execute(ret, resolution)
-        pos = result 
-        count += 1
+         draw(world, count)
+         starlings = opcl.execute(starlings, world)
+         print starlings[:,:2]
+         
+         world = form_world(starlings, resolution[0],resolution[1])        
+         # print world
+         
+         print count
+  
+         count += 1
